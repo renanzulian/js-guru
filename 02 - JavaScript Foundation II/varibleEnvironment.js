fe3cb7fc51f66b62f08824189a4072ac33467827
Renan Zulian <renanzulian@gmail.com>
2020-07-25 17:09:04 -0300

    About scope chain

--- 02 - JavaScript Foundation II/varibleEnvironment.js
+++ 02 - JavaScript Foundation II/varibleEnvironment.js
@@ -23,3 +23,48 @@ console.log(myVar)
 // execution context global -> false
 // Notice that it don't break. Each context is like a another "world"
 // and hav it's own properties (variables)
+
+
+// Scope chain
+// How can we know our available variables?
+// To this we only need to know what is the lexical scope of the function.
+// We can discover it it by eye. You just need to find where the function is written 
+
+var variable = 'An variable'
+
+function funcA(){
+    var variable = 'Another'
+    funcB()
+}
+
+function funcB(){
+    console.log(variable)
+}
+
+funcA()
+
+// Why it didn't printed? Because is the lexical scope that define the the available variables
+// and not the dynamic scope (where the function is called or by position in the call stack)
+// 
+// Scopes can be linked. It depends how the code is implemented.
+// Check this two example below
+// 1 - Global lexical environment
+// Here all function has only access the variables of the global (Whatever function can call one another)
+var x = 'Some value'
+
+function funcA() {
+    console.log(x)
+}
+
+function funcB() {
+    console.log(x)
+}
+
+// 2 - Function lexical environment
+// Here a function have access the data of the another function
+function main() {
+    return function example() {
+        console.log("Im' a function lexical environment in main")
+    }
+}
+
