9e555b779690334e290b0f2c3a270cdfbb9d2190
Renan Zulian <renanzulian@gmail.com>
2020-08-04 18:43:06 -0300

    JavaScript types lecture

---
+++ 03 - Types in JavaScript/types.js
@@ -0,0 +1,28 @@
+// There are 7 types in JavaScript
+
+// Primitive types
+typeof 5 // number
+typeof true // boolean
+typeof 'A small text' // string
+typeof undefined // undefined
+typeof null // object
+typeof Symbol('Just do it') // symbol
+// The primitive types is a data that only represents a single value
+
+// Non-primitive types
+typeof {} // object
+typeof [] // object
+typeof function(){} // function
+// Non-primitive types doesn't actually contain the value directly instead it has a reference similar to a pointer to somewhere
+
+// "Everything in JavaScript is an object"
+// Many things that we interact with directly in javascript such just string, numbers, booleans which
+// which are primitive and not object get a little but complicated by the fact that these primitive have
+// object wrappers like String, Number or Boolean
+
+true.toString() // It silently creates a wrapper object around this true
+Boolean(true).toString() // Something like this
+
+typeof Math // An standard built-in objects
+typeof Infinity // An standard built-in objects
+
